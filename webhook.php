794ba6b5eeafabbce44e4a03ce38a60fc406f965
codethejason <ihaveapet1@hotmail.com>
2015-12-24 15:02:15 -0500

    trying json way

--- webhook.php
+++ webhook.php
@@ -1,20 +1,22 @@
 <?php
 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_RETURNTRANSFER => 1,
-  CURLOPT_URL => 'https://api.github.com/repos/octocat/Hello-World/issues/229/comments',
-  CURLOPT_USERAGENT => 'PHP',
-  CURLOPT_POST => 1,
-  CURLOPT_POSTFIELDS => array(
-    "body" => "testing101",
-    "access_token" => "2d54013402c86c0e488b9bb2000ecbc2dedf35ff"
-  )
-)
-);
-print_r(curl_getinfo($curl));
-$resp = curl_exec($curl);
-print_r($resp);
-curl_close($curl);
+$data = array(    
+  "body" => "testing101",
+  "access_token" => "2d54013402c86c0e488b9bb2000ecbc2dedf35ff"
+);                                                                    
+$data_string = json_encode($data);                                                                                   
+                                                                                                                     
+$ch = curl_init('https://api.github.com/repos/octocat/Hello-World/issues/229/comments');                                                            
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($data_string))                                                                       
+);                                                                                                                   
+                                                                                                                     
+$result = curl_exec($ch);
+print_r($result);
+curl_close($ch);
 
 ?>
