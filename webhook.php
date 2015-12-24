f7dc8c5914978fce113cd5b0fa136d4b4e66642f
codethejason <ihaveapet1@hotmail.com>
2015-12-24 14:38:47 -0500

    revert to post

--- webhook.php
+++ webhook.php
@@ -5,15 +5,11 @@ curl_setopt_array($curl, array(
   CURLOPT_RETURNTRANSFER => 1,
   CURLOPT_URL => 'https://api.github.com/repos/octocat/Hello-World/issues/229/comments',
   CURLOPT_USERAGENT => 'PHP',
-  CURLOPT_POST => 0,
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type':'application/json',
-    'Authorization': 'token'
-  ),
- /* CURLOPT_POSTFIELDS => array(
+  CURLOPT_POST => 1,
+  CURLOPT_POSTFIELDS => array(
     "body" => "testing101",
     "access_token" => "2d54013402c86c0e488b9bb2000ecbc2dedf35ff"
-  )*/
+  )
 )
 );
 print_r(curl_getinfo($curl));
