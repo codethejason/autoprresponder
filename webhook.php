3ae479a47c4e5325645fd031b988e54ad1900bfc
codethejason <ihaveapet1@hotmail.com>
2015-12-24 17:12:11 -0500

    added webhook functionality

--- webhook.php
+++ webhook.php
@@ -1,22 +1,35 @@
 <?php
 
-$data = array(    
-  "body" => "testing101"
-);                                                                    
-$data_string = json_encode($data);                                                                                   
+if ($_POST['payload']) {
 
-$ch = curl_init('https://api.github.com/repos/octocat/Hello-World/issues/229/comments');                                                            
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');                                                                      
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',
-    'Authorization: token x'
-);                                                                                                                   
-                                                                                                                     
-$result = curl_exec($ch);
-print_r($result);
-curl_close($ch);
+  $payload = json_decode($_POST['payload']);
+  $action = $payload['action'];//since API alerts on other stuff like "assigned", "unassigned", "labeled", "unlabeled", "opened", "closed", or "reopened", or "synchronize"
+  
+  if($action == 'opened') { 
 
+    $pullrequestID = $payload['number'];
+    $username = $payload['pull_request']['user']['login'];
+
+    $url = 'https://api.github.com/repos/fossasia/gci15.fossasia.org/issues/'.$pullrequestID.'/comments';
+
+    $data = array(    
+      "body" => "http://".$username.".github.io/gci15.fossasia.org"
+    );                                                                    
+    $data_string = json_encode($data);                                                                                   
+
+    $ch = curl_init($url);                                                            
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');                                                                      
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json',
+        'Authorization: token x'
+    ));                                                                                                                   
+
+    $result = curl_exec($ch);
+    print_r($result);
+    curl_close($ch);
+  }
+}
 ?>
