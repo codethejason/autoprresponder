1e8f9ccc629104d45a428a1f4807cbcfee7c4a7c
codethejason <ihaveapet1@hotmail.com>
2015-12-24 16:53:02 -0500

    new authorization

--- webhook.php
+++ webhook.php
@@ -1,19 +1,18 @@
 <?php
 
 $data = array(    
-  "body" => "testing101",
-  "access_token" => "2d54013402c86c0e488b9bb2000ecbc2dedf35ff"
+  "body" => "testing101"
 );                                                                    
 $data_string = json_encode($data);                                                                                   
-                                                                                                                     
+
 $ch = curl_init('https://api.github.com/repos/octocat/Hello-World/issues/229/comments');                                                            
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');                                                                      
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',                                                                              
-    'Content-Length: ' . strlen($data_string))                                                                       
+    'Content-Type: application/json',
+    'Authorization: token x'
 );                                                                                                                   
                                                                                                                      
 $result = curl_exec($ch);
