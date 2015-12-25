eb59fd07dd76a3e4720c51775300ec2c4188151e
codethejason <ihaveapet1@hotmail.com>
2015-12-24 23:35:40 -0500

    securing webhook with key
    Key now working

--- webhook.php
+++ webhook.php
@@ -1,10 +1,10 @@
 <?php
 
-$payload = file_get_contents('php://input');
+$payloadJSON = file_get_contents('php://input');
 
-if ($payload) {
+if (isset($payloadJSON)) {
 
-  $payload = json_decode($payload, true);
+  $payload = json_decode($payloadJSON, true);
   $action = $payload['action'];//since API alerts on other stuff like "assigned", "unassigned", "labeled", "unlabeled", "opened", "closed", or "reopened", or "synchronize"
 
   if($action == 'opened' || $action == 'reopened') { 
@@ -13,30 +13,32 @@ if ($payload) {
     $username = $payload['pull_request']['user']['login'];
 
     $url = 'https://api.github.com/repos/codethejason/gci15.fossasia.org/issues/'.$pullrequestID.'/comments';
-
     $secretStuff = json_decode(file_get_contents('secret.json'), true);
     $token = $secretStuff['token'];
-    $key = $secretStuff['webhookkey'];
+    $key = "sha1=".hash_hmac('sha1', $payloadJSON, $secretStuff['githubkey']);
 
-    $httpheaders = array("Content-Type: application/json", "Authorization: token {$token}");
-    print_r($httpheaders);
-    $data = array(
-      "body" => "http://".$username.".github.io/gci15.fossasia.org"
-    );                                                                    
-    $data_string = json_encode($data);                                                                                   
+    if(isset($_SERVER['HTTP_X_HUB_SIGNATURE']) && $_SERVER['HTTP_X_HUB_SIGNATURE'] == $key) {
 
-    $ch = curl_init($url);                                                            
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');                                                                      
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $httpheaders);
+      $httpheaders = array("Content-Type: application/json", "Authorization: token {$token}");
 
-    $result = curl_exec($ch);
-    print_r($result);
-    curl_close($ch);
-    echo "Success!";
+      $data = array(
+        "body" => "http://".$username.".github.io/gci15.fossasia.org"
+      ); 
 
+      $data_string = json_encode($data);                                                                                   
+
+      $ch = curl_init($url);                                                            
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+      curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');                                                                      
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $httpheaders);
+
+      $result = curl_exec($ch);
+      curl_close($ch);
+      echo "Success! Commented for {$username} on pull request id {$pullrequestID}.";
+
+    }
   }
 }
 ?>
